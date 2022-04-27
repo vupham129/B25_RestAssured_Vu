6f711a8797c89edcf40a4a7e50bd35dd064b0b99
vupha <vupham129@gmail.com>
2022-04-27 14:59:36 -0700

    day6

--- src/test/java/com/cydeo/day6/HRPojoGetRequestTest.java
+++ src/test/java/com/cydeo/day6/HRPojoGetRequestTest.java
@@ -1,5 +1,6 @@
 package com.cydeo.day6;
 
+import com.cydeo.pojo.Employee;
 import com.cydeo.pojo.Region;
 import com.cydeo.utilities.HRTestBase;
 import io.restassured.http.ContentType;
@@ -25,10 +26,24 @@ public class HRPojoGetRequestTest extends HRTestBase {
         // we want to store second region under Region class object
         Region region2 = jsonPath.getObject("items[1]", Region.class);
 
-        System.out.println("region2.getRegion_id() = " + region2.getRegion_id());
-        System.out.println("region2.getRegion_name() = " + region2.getRegion_name());
-        System.out.println("region2.getLinks().get(0).getHref() = " + region2.getLinks().get(0).getHref());
+//        System.out.println("region2.getRegion_id() = " + region2.getRegion_id());
+//        System.out.println("region2.getRegion_name() = " + region2.getRegion_name());
+//        System.out.println("region2.getLinks().get(0).getHref() = " + region2.getLinks().get(0).getHref());
 
+
+    }
+
+    @Test
+    public void test2(){
+
+        JsonPath jsonPath = get("/employees").then().statusCode(200).extract().jsonPath();
+
+        Employee emp1 = jsonPath.getObject("items[0]", Employee.class);
+
+        System.out.println("emp1.getJobId() = " + emp1.getJobId());
+        System.out.println("emp1.getFirstname() = " + emp1.getFirstname());
+        System.out.println("emp1.getLastName() = " + emp1.getLastName());
+        System.out.println("emp1.getSalary() = " + emp1.getSalary());
 
     }
 
--- src/test/java/com/cydeo/pojo/Region.java
+++ src/test/java/com/cydeo/pojo/Region.java
@@ -1,5 +1,6 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,8 +11,11 @@ import java.util.List;
 @Setter
 @ToString
 public class Region {
-    private int region_id;
-    private String region_name;
+
+    @JsonProperty("region_id")
+    private int regionId;
+    @JsonProperty("region_name")
+    private String regionName ;
     private List<Link> links;
 
 
