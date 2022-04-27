018030126d991a83ebc7a73f809e4324437f59e4
vupha <vupham129@gmail.com>
2022-04-27 13:03:35 -0700

    day6

---
+++ src/test/java/com/cydeo/pojo/Spartan.java
@@ -0,0 +1,53 @@
+package com.cydeo.pojo;
+
+public class Spartan {
+    //to create pojo
+    //variables
+    //getter and setter
+    //toString
+
+    private int id;
+    private String name, gender;
+    private long phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String toString() {
+        return "Spartan{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
